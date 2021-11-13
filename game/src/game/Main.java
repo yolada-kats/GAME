7c1fb7d9d43e1916fbeb0e89b603373a3fde0c9b
gregchanikian <92597896+gregchanikian@users.noreply.github.com>
2021-11-13 14:29:06 +0200

    Update Main.java

--- game/src/game/Main.java
+++ game/src/game/Main.java
@@ -3,10 +3,7 @@ package game;
 public class Main {
 
 	public static void main(String[] args) {
-		//pou einai re paidia h main
-		//greg : nope
-		// TODO Auto-generated method stub
-		//paidia tha xalaseiiiiiiii aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+		
 
 	}
 
