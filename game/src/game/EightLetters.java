7b4ffb5a48cf7ac57a4a4ced1d4d1513d08f2b3d
IoannaGrb2 <94221919+IoannaGrb2@users.noreply.github.com>
2021-11-20 11:25:05 +0200

    Update EightLetters.java

--- game/src/game/EightLetters.java
+++ game/src/game/EightLetters.java
@@ -33,17 +33,15 @@ public class EightLetters implements ActionListener{
 		int n[] = new int[8];
 		int[] results = new int[8];
 		String[] question1 = new String[6];
-        private int count;
-        private int score=0;
-        private int tries=0;
+                private int count;
+                private int score=0;
+                private int tries=0;
         
 	public EightLetters(String[] word,String[] question1) {
 		
 		//set fields
-		this.count=count;
 		this.question1=question1;
 		this.word = word;
-		k=0;
 		createframe();
 		
 	}
@@ -108,26 +106,24 @@ public class EightLetters implements ActionListener{
 		  }  
 	  } 
 		
-		if(k==8) {
-		textfield.setVisible(false);
-		question.setVisible(false);
-		for(int i1=0;i1<8;i1++) {
-			buttons[i1].setVisible(false);
-		}
-	    createframe();
-	  }
+	 	if(k==8) {
+			textfield.setVisible(false);
+			question.setVisible(false);
+			for(int i1=0;i1<8;i1++) {
+				buttons[i1].setVisible(false);
+			}
+	       		createframe();
+	       }
 	  
-	  if(tries==3) {
-		    textfield.setVisible(false);
+	       if(tries==3) {
+		        textfield.setVisible(false);
 			question.setVisible(false);
 			for(int i1 =0;i1<8;i1++) {
 				buttons[i1].setVisible(false);
 			}
-		  count++;
-		  createframe();
-	  } 
-	 
-	  
+		       count++;
+	       	       createframe();
+	      } 
     }
 	
 	void createframe(){
@@ -140,68 +136,70 @@ public class EightLetters implements ActionListener{
 		 //if count=5 then display score level completed ,or not display frame with the next word
 		 if(count==5) {
 			 
-			  JPanel score_panel = new JPanel();
+		       //Create score panel and score label
+	        	  JPanel score_panel = new JPanel();
 			  JLabel score1 = new JLabel("Score "+score+"/5");
 			  score_panel.add(score1);
-			  score_panel.setBounds(0,0,900,4);
-	          score_panel.setBackground(Color.orange);
-	          score1.setForeground(Color.WHITE);
-	          score1.setFont(new Font("Ink Free",Font.BOLD,75));
-		      score1.setHorizontalAlignment(JLabel.CENTER);
-	 	      score1.setForeground(Color.black);	
-	          score1.setBackground(Color.YELLOW);
-	 	      score1.setLayout(new BorderLayout());	
-			  frame.add(score_panel);
+		          score_panel.setBounds(0,0,900,4);
+	                  score_panel.setBackground(Color.orange);
+	                  score1.setForeground(Color.WHITE);
+	                  score1.setFont(new Font("Ink Free",Font.BOLD,75));
+		          score1.setHorizontalAlignment(JLabel.CENTER);
+	 	          score1.setForeground(Color.black);	
+	                  score1.setBackground(Color.YELLOW);
+	 	          score1.setLayout(new BorderLayout());	
+		          frame.add(score_panel);
 			 
 		 }else {
 	
-		        //Create frame
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setSize(1100,800);
-				frame.getContentPane().setBackground(new Color(50,50,50));
-				frame.setLayout(new BorderLayout());
-				frame.setVisible(true);
+		       //Create frame
+			  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		          frame.setSize(1500,800);
+			  frame.getContentPane().setBackground(new Color(50,50,50));
+			  frame.setLayout(new BorderLayout());
+			  frame.setVisible(true);
 		 
 		   	  
-		        //Create question label and panel
-				question = new JLabel(question1[count]);
-				question.setFont(new Font("Ink Free",Font.BOLD,75));
-				question.setHorizontalAlignment(JLabel.CENTER);
-		   	    question.setForeground(Color.WHITE); 
-		   	    question_panel.setBackground(Color.BLUE);
-		        question_panel.add(question);
+		       //Create question label and panel
+			  question = new JLabel(question1[count]);
+		          question.setFont(new Font("Ink Free",Font.BOLD,50));
+			  question.setHorizontalAlignment(JLabel.CENTER);
+		   	  question.setForeground(Color.WHITE); 
+		   	  question_panel.setBackground(Color.BLUE);
+		          question_panel.add(question);
 		        
 		       //Create textfield label and panel
-                  textfield = new JLabel("__ __ __ __ __ __ __ __"); 
-			      textfield.setBounds(0,0,900,4);
+                          textfield = new JLabel("__ __ __ __ __ __ __ __"); 
+			  textfield.setBounds(0,0,900,4);
 		          textfield.setBackground(Color.orange);
 		          textfield.setForeground(Color.WHITE);
 		          textfield.setFont(new Font("Ink Free",Font.BOLD,75));
-			      textfield.setHorizontalAlignment(JLabel.CENTER);
-		 	      textfield.setForeground(Color.black);	
+			  textfield.setHorizontalAlignment(JLabel.CENTER);
+		 	  textfield.setForeground(Color.black);	
 		          title_panel.setBackground(Color.YELLOW);
-		 	      textfield.setLayout(new BorderLayout());	
-				  title_panel.add(textfield);
+		 	  textfield.setLayout(new BorderLayout());	
+	                  title_panel.add(textfield);
 
 		        
-		 	    //Split the word and create an array with the letters of the word
-				array = word[count].split("");	 
+		       //Split the word and create an array with the letters of the word
+		           array = word[count].split("");	 
 				
-				//Create an array of random numbers from 1 to 7;
-				for(int i1=0;i1<8;i1++) {
-					n[i1]=i1;	
-				}
-				//Swap the index of the array n[i] with the index of n[randomIndex] so that the letters appear shuffled
-		        Random random = new Random();
-				for (int i=0;i<n.length;i++) {
+		       //Create an array of random numbers from 1 to 7;
+			    for(int i1=0;i1<8;i1++) {
+			     	n[i1]=i1;	
+			     }
+			 
+		       //Swap the index of the array n[i] with the index of n[randomIndex] so that the letters appear shuffled
+		              Random random = new Random();
+		              for (int i=0;i<n.length;i++) {
 					int randomIndex = random.nextInt(n.length);
 					int temp = n[randomIndex];
 					n[randomIndex] = n[i];
 					n[i] = temp;
-				}
+			      }
 				
-				//Create buttons 
-				for(int i=0;i<8;i++) {
+			//Create buttons 
+			       for(int i=0;i<8;i++) {
 					buttons[i] = new JButton(array[n[i]]);
 					button_panel.add(buttons[i]);
 					buttons[i].setFont(new Font("MV Boli",Font.BOLD,120));
@@ -211,7 +209,7 @@ public class EightLetters implements ActionListener{
 					buttons[i].addActionListener(this);
 				 }
 				
-				//Add panels to frame 
+			//Add panels to frame 
 				button_panel.setBackground(Color.WHITE);
 				title_panel.setBounds(0,0,800,100);
 				frame.add(title_panel,BorderLayout.CENTER);
