3aea56a76972b24f8e3ddb82e9af070bd9fc4104
IoannaGrb2 <t8200025@aueb.gr>
2022-01-04 12:36:27 +0200

    cnjskbc

--- game/src/game/Main.java
+++ game/src/game/Main.java
@@ -11,18 +11,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
 		Map map = new Map(frame, 0);
-		//Question question1 = new Question(1,frame);
 		
 		
 		//Menu men = new Menu(0);
-		//Question question2 = new Question(2,frame);
-		//Question question3 = new Question(3,frame);
 		
-		
-		//Mono gia thn polh sparti mexri stigmhs 8a ginei kai gia tis alles 2 poleis to idio pragma me allous pinakes
-	       String[] word = {"LEONIDAS","LYKURGOS","PATRIOTS","LUXURIES","EXERCISE"};
-	       String[] q = {"He was the king of Sparta","He invented the laws in Sparta","The Spartans became known as brave and","The Spartans did not like","Women in ancient Sparta attached great importance to physical"};
-	      // Findtheword sparta = new Findtheword(word,q);
 
 	
 	       
