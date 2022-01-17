bc482b0adbcb9928f6f907094a598a63d60c35cd
IoannaGrb2 <t8200025@aueb.gr>
2022-01-17 19:45:56 +0200

    hjvhj

--- game/src/game/Map.java
+++ game/src/game/Map.java
@@ -50,8 +50,8 @@ public class Map extends JFrame {
 	    frame.setBackground(new java.awt.Color(153, 204, 255));
 		frame.setLayout(new BorderLayout());
 		frame.setResizable(false);
-		ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
-		frame.setIconImage(logo.getImage());
+		//ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
+		//frame.setIconImage(logo.getImage());
 		if (count == 0) {
 			addgif("SANTA1.gif");
 		}
